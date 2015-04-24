8dbba39a56042ef0656710fff4ecac61f7c939b1
goldenratio1618 <aaatanas@yahoo.com>
2015-04-23 20:10:40 -0400

    Genetic AI v1 Complete
    Version 1 of genetic Ai has been implemented (side project; not related
    to neuron networks project). Not yet tested for either bugs or
    performance.

--- 2048-python/genetic_AI.py
+++ 2048-python/genetic_AI.py
@@ -2,6 +2,8 @@ from game import *
 import random as rand
 
 class GeneticAI:
+    # Determines how fast we hone in on the solution
+    lambda_change_factor = 0.999
     def __init__(self, b=None, row_arr=None, col_arr = None, s=None):
         if b is None:
             self.board = Board2048(seed=s)
@@ -17,6 +19,8 @@ class GeneticAI:
             self.col_arr = col_arr
         self.best_row_arr = row_arr
         self.best_col_arr = col_arr
+        self.best_score = 0
+        self.lambda = 1.0
 
     @staticmethod
     def rand_array():
@@ -25,57 +29,53 @@ class GeneticAI:
             arr[i] = rand.random() * 2 - 1
         return arr
         
-    def move():
-        dir_f = 
-        
-    def train(num_games):
-        
-    """ Computes all future outcomes up to a certain depth
-
-        Returns a tuple consisting of a list of all board outcomes,
-        plus the direction of motion yielding the maximum score across all
-        outcomes. Does not take random spawns into account
-    """
     @staticmethod
-    def _calc_move(board, iter):
-        if iter == 0:
-            return ([board], 0)
-        board_arr = []
-        # maximum score attainable, together with the direction to move in
-        dir = (-1, 0)
-        for i in gs:
-            # make new Board2048 object so we don't change original copy
-            new_board = Board2048(np.copy(board.grid))
-            # move it in the given direction
-            new_board.move_without_spawn(i)
-            # get tree of moves one level down
-            sub_board_arr = GreedyAI._calc_move(new_board, iter - 1)[0]
-            # get scores (we maximize this)
-            scores = list(map(lambda b: b.score, sub_board_arr))
-            max_s = max(scores)
-            if(max_s > dir[0]):
-                dir = (max_s, i)
-            board_arr += sub_board_arr
-        return (board_arr, dir[1])
+    def calc_move():
+        dir_f = np.dot(np.dot(self.col_arr, self.board.grid), self.row_arr)
+        # normalize so that we don't always go down later in the game
+        dir_f /= np.sum(self.board.grid)
+        if dir_f <= 0:
+            return 0
+        if dir_f >= 3:
+            return 3
+        else:
+            return round(dir_f)
+        
+    """ Moves the board according to the genetic AI's algorithm.
 
-    """ Moves the board according to the greedy AI's algorithm.
-
-        Returns true if AI's move was possible
+        Returns the move that the AI made, or -1 if the game was over.
         If AI's move wasn't possible, tries to move in the following order:
         right, then up, then left, then down
     """
     def move(self):
         # game is over; do nothing
         if(self.board.game_over()):
-            return False
+            return -1
         # calculate AI's move
-        dir = GreedyAI._calc_move(self.board, self.iters)[1]
+        dir = GeneticAI._calc_move(self.board, self.iters)[1]
         # try to move in that direction
         if(self.board.move(dir)):
-            return True
+            return dir
         # if the move failed, try all possible moves in order
         for dir in gs:
             if(self.board.move(dir)):
-                return False
+                return dir
         # should never happen
         raise RuntimeError("ERROR: No moves possible, but game not over")
+    
+    """ Plays an entire game, until it loses; returns the score """
+    def play(self):
+        while self.move() != -1:
+            # do nothing
+        return self.board.score
+
+    def train(self, num_games):
+        for i in range(num_games):
+            score = self.play()
+            if score > self.best_score:
+                self.best_score = score
+                self.best_row_arr = self.row_arr
+                self.best_col_arr = self.col_arr
+            self.row_arr = self.best_row_arr + lambda * rand_array()
+            self.col_arr = self.best_col_arr + lambda * rand_array()
+            lambda *= lambda_change_factor
