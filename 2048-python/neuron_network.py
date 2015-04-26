b6eada2fee78ca914a73f56f0f7ef72f25b71189
goldenratio1618 <aaatanas@yahoo.com>
2015-04-26 01:10:21 -0400

    Minor feature fix
    Neuron network now takes an array of lengths rather than neurons as
    input.

--- 2048-python/neuron_network.py
+++ 2048-python/neuron_network.py
@@ -3,13 +3,13 @@ import numpy as np
 
 class NeuronNetwork:
     """ Constructs a neuron network from an array of neuron arrays """
-    def __init__(neuron_arrs, weights=None):
+    def __init__(neuron_arrs_len, weights=None):
         if weights != None:
             self.weights = weights
         else:
             self.weights = []
             for i in range(len(neuron_arrs) - 1):
-                len1 = len(neuron_arrs[i])
-                len2 = len(neuron_arrs[i+1])
+                len1 = neuron_arrs_len[i]
+                len2 = neuron_arrs_len[i+1]
                 self.weights.append(np.ones((len1, len2)))
     
