ebc620bf241d1dcb00d629c240f5c881ef647a17
Mark Ting (Eunomia) <mat3049@gmx.com>
2015-04-29 06:20:17 -0400

    Added error calculation in back_propagation
    Still need to add weight adjustment to back_propagation.

--- 2048-python/neural_network.py
+++ 2048-python/neural_network.py
@@ -15,10 +15,9 @@ class NeuralNetwork:
         self.n_hidden = n_hidden
         self.n_layers = n_layers
 
-        self.total_layers = n_layers + 2
-
-        # Check if hidden layers exist
-        self.hidden_layers_exist = (n_layers != 0)
+        self.total_layers = n_layers + 2            # helps indexing later
+        self.hidden_layers_exist = (n_layers != 0)  # saves work later
+        self.test = 0
 
         # Set clamping and gradient functions
         if function_set is not None:
@@ -35,38 +34,49 @@ class NeuralNetwork:
             self.clamp = tanh
             self.grade = tanh_prime
 
-        # Initialize interconnection weight values
+        # Initialize weight values for node interconnections
         self.weights = []
 
         if self.hidden_layers_exist:
-            # Input-Hidden matrix
+            # Input-Hidden weights
             self.weights.append(np.random.rand(self.n_hidden, self.n_in))
 
-            # Hidden-Hidden matrices
-            for i in range(self.n_layers):
+            # Hidden-Hidden weights
+            for i in range(self.n_layers):  # handles multiple layers
                 self.weights.append(np.random.rand(self.n_hidden,
                                                    self.n_hidden))
 
-            # Hidden-Output matrix
+            # Hidden-Output weights
             self.weights.append(np.random.rand(self.n_out, self.n_hidden))
 
         else:
             # Input-Output matrix
             self.weights.append(np.random.rand(self.n_out, self.n_in))
 
-        # Initialize propogation value arrays
+        # Initialize propogation and error value arrays
         self.prop_values = []
+        self.error_values = []
 
-        self.prop_values.append(np.zeros(self.n_in))  # input values
+        self.prop_values.append(np.zeros(self.n_in))  # no need for error (in!)
 
         if self.hidden_layers_exist:
+            # Hidden layer values and errors
             for i in range(self.n_layers):
                 self.prop_values.append(np.zeros(self.n_hidden))
+                self.error_values.append(np.zeros(self.n_hidden))
 
-        self.prop_values.append(np.zeros(self.n_out))  # output values
+        # Output values and errors
+        self.prop_values.append(np.zeros(self.n_out))
+        self.error_values.append(np.zeros(self.n_out))
 
+        self.error_values.reverse  # sets output first (for error evaluation)
+
+        # Sanity checks!
         assert len(self.prop_values) == self.total_layers, \
-            "Propogation array size mismatch!"
+            "Propogation array is of the wrong size!"
+
+        assert len(self.error_values) == self.total_layers - 1, \
+            "Error array is of the wrong size!"
 
     # TODO: DOUBLE CHECK THIS WORKS
     def propogate(self, inputs):
@@ -89,24 +99,55 @@ class NeuralNetwork:
         # Evaluate input layer (assigns input values to propogation array)
         print("Evaluating input layer")
         for i in range(self.n_in - 1):
-            (self.prop_values[0])[i] = inputs[i]
+            self.prop_values[0][i] = inputs[i]
 
         # Evaluate hidden layers
-        print("Evaluating hidden layers")
-        for i in range(self.n_layers):  # for each layer...
+        print("Evaluating hidden layer(s)")
+        for i in range(self.n_layers):  # for each hidden layer...
             for j in range(self.n_hidden):  # for each node...
-                signal = np.dot((self.weights[i])[j, :],
-                                (self.prop_values[i]))
-                (self.prop_values[i + 1])[j] = self.clamp(signal)
+                signal = np.dot(self.weights[i][j, :],
+                                self.prop_values[i])
+                self.prop_values[i + 1][j] = self.clamp(signal)
 
         # Evaluate output layer
         print("Evaluating output layer")
         for i in range(self.n_out):
-            signal = np.dot((self.weights[last_weight])[i, :],
+            signal = np.dot(self.weights[last_weight][i, :],
                             self.prop_values[last_layer - 1])
-            (self.prop_values[last_layer])[i] = self.clamp(signal)
+            self.prop_values[last_layer][i] = self.clamp(signal)
 
         return(self.prop_values[last_layer])
+
+    def back_propogate(self, training, learn_rate):
+        """ Calculate error after a propogation run and adjust weights based
+            on provided training data.
+
+            Args:
+                training - set of expected outputs
+
+        """
+        last_layer = self.total_layers - 1
+        h_start = self.total_layers - 2  # start hidden layers reverse index
+
+        # Calculate output layer error
+        print("Evaluating output error")
+        for i in range(self.n_out):
+            error = training[i] - (self.prop_values[last_layer])[i]
+            self.error_values[0][i] = \
+                error * self.grade((self.prop_values[last_layer])[i])
+
+        # Calculate hidden layer error
+        print("Evaluating hidden layer error")
+        for i in range(self.n_layers):  # for each hidden layer...
+            for j in range(self.n_hidden):
+                error = np.dot(self.error_values[0], self.weights[i + 1][j, :])
+                self.error_values[i + 1][j] = \
+                    error * self.grade(self.prop_values[h_start - i][j])
+
+        # Update output weights
+        # Update hidden weights
+        # Update input weights
+        return self.error_values
 
     def load_weights(self, weights_in):
         """ Load weights from a provided file into the neural network """
@@ -122,7 +163,3 @@ class NeuralNetwork:
 
     def set_bias(self, bias):
         (self.prop_values[0])[self.n_in - 1] = bias
-
-    def back_propogate(self, training_data, learn_rate, error_bound):
-        # USE ERROR AND UPDATE!
-        return self.weights
