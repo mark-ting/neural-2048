dcb0de858cd30a66619427356703666ff6d324be
Mark Ting (Eunomia) <mat3049@gmx.com>
2015-04-30 14:22:59 -0400

    Modified corner heuristic
    Modified corner function in heuristics to be more in line with the other
    functions.
    
    Corner now acts on a standard 2D grid rather than a Board2048--use
    expose first to retain original functionality.

--- 2048-python/heuristics.py
+++ 2048-python/heuristics.py
@@ -19,13 +19,13 @@ x = Board2048(np.array([[f, t, s, t], [t, e, t, f],
 class Heuristics:
 
     def expose_val(array):
-        """ Exposes values of 2048 objects. """
+        """ Expose values of 2048 objects. """
         def f(x): return x.val
         f = np.vectorize(f)
         return f(array)
 
     def ascending(array):
-        """ Verifies array is in ascending order.
+        """ Verify array is in ascending order.
 
             Horizontal arrays: left -> right
             Vertical arrays: top -> bottom
@@ -54,19 +54,19 @@ class Heuristics:
 
     # PLACEHOLDER -- NEED TO RETURN THE COORDINATES NOT THE NUMBER
     def max_val(array):
-        """ Return the value of the largest (object) on the board.
+        """ Return the value of the largest number on the board.
 
             DOES NOT YET:
             location...in the form of a zero-indexed tuple (x, y)
         """
         return np.amax(array)
 
-
-    def corner(board2048, corner=0):
-        """ Return the value of the corner object on a 2048 board.
+    def corner(grid, corner=0):
+        """ Return a corner element (of a 2D grid).
+            Can handle non-square grids (n by m is OK)
 
             Args:
-                board2048 (Object2048): game board to analyze
+                grid: grid to analyze
                 corner (int): specifies which corner to look in
                   0 -- Top-Left
                   1 -- Top-Right
@@ -76,21 +76,20 @@ class Heuristics:
 
         """
 
-        grid = board2048.grid
         max_dim = np.shape(grid)
         X = max_dim[0] - 1  # right-most coordinate
         Y = max_dim[1] - 1  # upper-most coordinate
 
         if corner == 0:
-            corner_obj = (grid[0, 0])  # Top-Left
+            corner_obj = grid[0, 0]  # Top-Left
 
         elif corner == 1:
-            corner_obj = (grid[X, 0])  # Top-Right
+            corner_obj = grid[X, 0]  # Top-Right
 
         elif corner == 2:
-            corner_obj = (grid[X, Y])  # Bottom-Right
+            corner_obj = grid[X, Y]  # Bottom-Right
 
         elif corner == 3:
-            corner_obj = (grid[0, Y])  # Bottom-Left
+            corner_obj = grid[0, Y]  # Bottom-Left
 
         return corner_obj
